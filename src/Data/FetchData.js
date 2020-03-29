eb1d46a70e9e048ad95e14dcb2634e2d107a5052
parastooveisi <parastoo.veisi@gmail.com>
2020-03-28 18:30:53 -0600

    Fetch Data

---
+++ src/Data/FetchData.js
@@ -0,0 +1,13 @@
+import readingsData from './readings';
+import sensorsData from './sensors';
+
+//ُ This file can be modified to be used with API
+
+function readings(){
+  return readingsData
+}
+function sensors(){
+  return sensorsData
+}
+
+export{readings, sensors};
