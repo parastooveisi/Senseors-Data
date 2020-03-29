1150dacfdea7d2c9e17ad11703df59fa5ac76881
parastooveisi <parastoo.veisi@gmail.com>
2020-03-28 18:48:41 -0600

    Create Table Component

---
+++ src/Components/Table.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Table extends Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+export default Table;
