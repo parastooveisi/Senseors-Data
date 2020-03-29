e503fe5654803ecea88e9c24da82e7f293111e2c
parastooveisi <parastoo.veisi@gmail.com>
2020-03-28 21:04:54 -0600

    Render Table Component

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -1,10 +1,55 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import readings from "../Data/readings";
+import sensors from "../Data/sensors";
+import { Table } from "semantic-ui-react";
 
-class Table extends Component {
+export default class Tables extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getHeader = this.getHeader.bind(this);
+    this.getRowsData = this.getRowsData.bind(this);
+    this.getKeys = this.getKeys.bind(this);
+  }
+
+  getKeys = function() {
+    return Object.keys(this.props.data[0]);
+  };
+
+  getHeader = function() {
+    var keys = this.getKeys();
+    return keys.map((key, index) => {
+      return <th key={key}>{key.toUpperCase()}</th>;
+    });
+  };
+
+  getRowsData = function() {
+    var items = this.props.data;
+    var keys = this.getKeys();
+    return items.map((row, index) => {
+      return (
+        <tr key={index}>
+          <RenderRow key={index} data={row} keys={keys} />
+        </tr>
+      );
+    });
+  };
+
   render() {
-    return <div>Hello</div>;
+    return (
+      <div>
+        <Table singleLine>
+          <Table.Header>
+            <tr>{this.getHeader()}</tr>
+          </Table.Header>
+          <tbody>{this.getRowsData()}</tbody>
+        </Table>
+      </div>
+    );
   }
 }
 
-export default Table;
+const RenderRow = props => {
+  return props.keys.map((key, index) => {
+    return <td key={props.data[key]}>{props.data[key]}</td>;
+  });
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import readings from "./Data/readings";
+import sensors from "./Data/sensors";
+import { formatToTimeZone } from "date-fns-timezone";
 import Table from "./Components/Table";
+import dateFormat from "dateformat";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      readings_data: readings,
+      sensors_data: sensors
+    };
+  }
+
   render() {
     return (
       <div>
-        <Table />
+        <Table data={this.state.sensors_data} />
+        <Table data={this.state.readings_data} />
       </div>
     );
   }
